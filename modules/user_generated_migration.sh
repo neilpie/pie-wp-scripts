2136ffde516f3f0952e678d9a74137368d9afec1
root <root@lamp-1.(none)>
2012-07-29 15:54:35 +0100

    switched uploads migrationg to 'user generated migration' and included htacces file

--- modules/user_generated_migration.sh
+++ modules/user_generated_migration.sh
@@ -2,3 +2,6 @@
 
 echo "Copying uploads..."
 cp -R $source_dir/content/uploads/* $destination_dir/content/uploads
+
+echo "Copying .htaccess file"
+cp -R $source_dir/.htaccess $destination_dir/.htaccess
