028de9328310d5e448ad069365fa10f803b85fc7
root <root@lamp-1.(none)>
2012-07-29 14:17:15 +0100

    cleaning up thummbnail for excerpts plugin

--- staging_to_production.all_files.single.sh
+++ staging_to_production.all_files.single.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
 MYDIR=`dirname $0`
+
+echo "loading $MYDIR/modules/get_project_name.sh"
 source $MYDIR/modules/get_project_name.sh
+echo "loading $MYDIR/modules/$project_name.sh"
 source $MYDIR/conf/$project_name.sh
 
+echo "loading $MYDIR/modules/single_check.sh"
 source $MYDIR/modules/single_check.sh
 
+echo "loading $MYDIR/modules/staging_to_production.sh"
 source $MYDIR/modules/staging_to_production_setup.sh
 
+echo "loading $MYDIR/modules/full_file_migration.sh"
 source $MYDIR/modules/full_files_migration.sh
+echo "loading $MYDIR/modules/set_permissions.sh"
 source $MYDIR/modules/set_permissions.sh
 
+echo "loading $MYDIR/modules/update_public_symlink.sh"
 source $MYDIR/modules/update_public_symlink.sh
 
 
