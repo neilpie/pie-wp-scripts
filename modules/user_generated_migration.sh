34ddc28d2e381b480df456a761e9ac1f0ca1e423
root <root@lamp-1.(none)>
2012-07-29 16:10:09 +0100

    cleaned up uploads import

--- modules/user_generated_migration.sh
+++ modules/user_generated_migration.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "Copying uploads..."
-cp -R $source_dir/content/uploads/* $destination_dir/content/uploads
+rm -fR $destination_dir/content/uploads
+cp -R $source_dir/content/uploads $destination_dir/content
 
 echo "Copying .htaccess file"
 cp -R $source_dir/.htaccess $destination_dir/.htaccess
