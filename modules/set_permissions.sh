8e936a4196962c5b7980dfef243a36ab91ec0bfd
Neil Pie <neil@resolutioncreative.co.uk>
2012-08-02 22:38:51 +0100

    changed permissions scripts to be more local

--- modules/set_permissions.sh
+++ modules/set_permissions.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 echo "updating all file permissions"
-chown -R $secure_owner:$developer_group $project_dir
-chown -R $writeable_owner:$developer_group $project_dir/staging
-chown -R $writeable_owner:$developer_group $project_dir/production.$project_name.*/content/uploads
-find $project_dir -type d -exec chmod 775 {} \;
-find $project_dir -type f -exec chmod 664 {} \;
-find $project_dir/pie-wp-scripts -type f -exec chmod 770 {} \;  
+chown -R $secure_owner:$developer_group $project_dir/production.$project_name.$timestamp
+chown -R $writeable_owner:$developer_group $project_dir/production.$project_name.$timestamp/content/uploads
+find $project_dir/production.$project_name.$timestamp -type d -exec chmod 775 {} \;
+find $project_dir/production.$project_name.$timestamp -type f -exec chmod 664 {} \;
 
---
+++ set_all_permissions.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+secure_owner=root
+developer_group=lewis
+writeable_owner=www-data
+project_dir=/home/lewis/wordpress-central
+echo "You are about to update ALL file permissions in WordPress-central."
+
+read -p "This may take some time. Press enter to proceed or Ctrl + C to exit"
+echo "updating all file permissions"
+
+chown -R $secure_owner:$developer_group $project_dir
+chown -R $writeable_owner $project_dir/{staging,production.*/content/uploads}
+
+find $project_dir -type d -exec chmod 775 {} \;
+find $project_dir -type f -exec chmod 664 {} \;
+find $project_dir/pie-wp-scripts -type f -exec chmod 770 {} \;
