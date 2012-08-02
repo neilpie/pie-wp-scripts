bd984e9a6cb47fcaedeec428d5e53270cb5d4193
ResolutionCreative <support@resolutioncreative.co.uk>
2012-08-02 21:22:42 +0100

    updating defaults

--- conf/project_config.sample.sh
+++ conf/project_config.sample.sh
@@ -7,18 +7,18 @@ fi
 timestamp=`date +%Y-%m-%d_%H.%M.%S_%Z`
 
 #The path to your project directory. This is the directory that contains the staging environment, the scripts directory and the tmp directory 
-home_dir=/path
-project_dir=$home_dir/path
+home_dir=/home/client_name
+project_dir=$home_dir/wordpress-central
 
 #These are the strings that we need to swap out when moving data between installations. These MUST conatin the same amount of characters
-staging_db_string=project.stg
-production_db_string=pproject.com
+staging_db_string=staging.domain
+production_db_string=live.domain
 master_db_user=wp_staging
 
 #These are the file permissions settings for production & staging 
 writeable_owner=www-data
 secure_owner=root
-developer_group=devgroup
+developer_group=client_name
 
 mysql_path=
 
@@ -28,6 +28,6 @@ mysql_path=
 #single_or_multi=multi
 
 #for single & multisite installs 
-staging_db_name=project_staging
-production_db_name=project_production
+staging_db_name=project_name_staging
+production_db_name=project_name_production
 single_or_multi=single
--- modules/get_project_name.sh
+++ modules/get_project_name.sh
@@ -1,4 +1,4 @@
 #!/bin/bash/
-read -p "Which project would you like to work with? "
+read -p "Which li ve domainwould you like to work with? "
 echo " "
 project_name=$REPLY
