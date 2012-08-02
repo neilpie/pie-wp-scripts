68c13f9d8cdbada3c37b864182f27b1e28f8c44d
Neil Pie <neil@resolutioncreative.co.uk>
2012-08-02 22:53:58 +0100

    making htaccess a writeable file in production

--- modules/set_permissions.sh
+++ modules/set_permissions.sh
@@ -2,7 +2,7 @@
 
 echo "updating all file permissions"
 chown -R $secure_owner:$developer_group $project_dir/production.$project_name.$timestamp
-chown -R $writeable_owner:$developer_group $project_dir/production.$project_name.$timestamp/content/uploads
+chown -R $writeable_owner:$developer_group $project_dir/production.$project_name.$timestamp/{content/uploads,.htaccess}
 find $project_dir/production.$project_name.$timestamp -type d -exec chmod 775 {} \;
 find $project_dir/production.$project_name.$timestamp -type f -exec chmod 664 {} \;
 
